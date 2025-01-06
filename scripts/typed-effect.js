fd1ad897dfd4b49531d29fe4207e7ff492263d48
jancris100 <73757410+jancris100@users.noreply.github.com>
2025-01-06 16:47:20 -0600

    sections primer deploy

---
+++ scripts/typed-effect.js
@@ -0,0 +1,18 @@
+const typed = new Typed(".typed", {
+    strings: [
+        '<p class="developer">Full Stack Developer</p>',
+        '<p class="developer">FrontEnd Developer</p>',
+        '<p class="developer">BackEnd Developer</p>'
+    ],
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html'
+});
